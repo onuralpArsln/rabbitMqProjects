c200abd2303a573cbc0b2a6bcc577f0d01a95a73
hackerFaruk <onuralparslan97@gmail.com>
2024-07-04 11:58:41 +0300

    added mongo db parts

--- p3_mongoDB/mongoConnectionCheck.py
+++ p3_mongoDB/mongoConnectionCheck.py
@@ -18,3 +18,57 @@ try:
     print("Pinged your deployment. You successfully connected to MongoDB!")
 except Exception as e:
     print(e)
+
+
+# database oluşumu 
+
+baseDb = client["basedatabase"]
+
+print("Database adı ->")
+#sistemdeki databaseleri gör 
+print(client.list_database_names())
+
+#bir koleksiyon oluştur 
+logCol = baseDb["logs"]
+
+# koleksiyonları kontrol et 
+
+print(baseDb.list_collection_names())
+
+
+# koleksiyona bir  ekleme yap 
+def addOne():
+    dailyLog = { "date": "today", "request": "reachingDb" }
+    x = logCol.insert_one(dailyLog)
+
+# koleksiyona toplu  ekleme yap 
+def addMany():
+    dailyLogs = [
+  { "date": "yesterday", "request": "create"},
+  { "date": "yesterday", "request": "create"},
+  { "date": "yesterday", "request": "create"},
+  { "date": "yesterday", "request": "create"},
+  { "date": "yesterday", "request": "update"},
+  { "date": "yesterday", "request": "update"},
+  { "date": "today", "request": "update"},
+  { "date": "today", "request": "delete"},
+  { "date": "today", "request": "delete"},
+  { "date": "today", "request": "read"},
+  { "date": "today", "request": "read"},
+  { "date": "today", "request": "read"}
+]
+    x = logCol.insert_many(dailyLogs)
+
+# içindekileri gör 
+def seeCol():
+
+    for x in logCol.find():
+        print(x)
+
+# arama yap 
+def search():
+    query = { "request": "read" }
+    logDoc = logCol.find(query)
+    for x in logDoc:
+        print(x)
+
